fc90a365e3ca96538df8dc6365b1d87826a18942
Andry925 <andry925@ukr.net>
2024-11-15 12:10:18 +0200

    feat:write initial java methid

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+class Main {
+
+    
+    public static void main(String[] args) {
+        // Create an array of Car objects with sample data
+       
+        };
+
+       
+}
